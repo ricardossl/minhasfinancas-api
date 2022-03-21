b9d93ebfade413081f9f11dee59575d7af50d4f5
Ricardo Saul <ricardo.saul@gmail.com>
2022-03-21 10:48:36 -0300

    Finalizando commit

--- src/main/java/com/ricardo/minhasfinancas/MinhasfinancasApplication.java
+++ src/main/java/com/ricardo/minhasfinancas/MinhasfinancasApplication.java
@@ -2,18 +2,9 @@ package com.ricardo.minhasfinancas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableWebMvc
-public class MinhasfinancasApplication implements WebMvcConfigurer {
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS");
-	}
-
+public class MinhasfinancasApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MinhasfinancasApplication.class, args);
 	}
